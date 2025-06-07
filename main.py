e72d8ef9324962648c08c0f5854619f5b4695d68
Mykhailo <102682204+skl30misha@users.noreply.github.com>
2025-06-07 21:48:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,5 @@ def index():
 
 # ==== Запуск ====
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
